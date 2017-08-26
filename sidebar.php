75c13939f9ac4562ebc6746f8287b432a99182f2
phanchikhang <phanchikhang7@gmail.com>
2017-08-26 14:59:58 +0700

    pckhang

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,7 @@
           <li><a href="#">Quản lý sinh viên</a></li>
           <li><a href="#">Quản lý giảng viên</a></li>
           <li><a href="#">Quản lý điểm</a></li>
+          <li><a href="#">Quản lý học phần</a></li>
           <li><a href="#">Thống kê</a></li>
         </ul>
       </div>
