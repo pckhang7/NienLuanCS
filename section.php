8e4a0da237db4ff084a0c993dd8713e2e3a71385
phanchikhang <phanchikhang7@gmail.com>
2017-08-27 17:15:58 +0700

    pckhang

--- section.php
+++ section.php
@@ -14,16 +14,61 @@
       </div>
       <div class="section-content">
         <div class="profile">
-          <p>Logo</p>
-          <p>Logo</p>
-          <p>Logo</p>
-          <p>Logo</p>
-          <p>Logo</p>
-          <p>Logo</p>
-          <p>Logo</p>
-          <p>Logo</p>
+          <p><div id="left">Logo: </div><div id="right"><img src="img/logo/logo.png" style="width:100px;height:100px"></img></div></p>
+          <p><div id="left">Tên trường: </div><div id="right">Đại Học Cần Thơ</div></p>
+          <p><div id="left">Địa chỉ: </div><div id="right">đường 3/2, quận Ninh Kiều, TP Cần Thơ</div></p>
+          <p><div id="left">Số điện thoại(liên hệ): </div><div id="right">(84-292) 3832663</div></p>
+          <p><div id="left">Fax: </div><div id="right">(84-292) 3838474</div></p>
         </div>
         <div class="mark">
+          <h4>Bảng tính thang điểm</h4>
+          <table border="1">
+            <tr>
+              <th>Điểm chữ</th>
+              <th>Điểm số(1-4)</th>
+              <th>Điểm phần trăm(1-100)</th>
+            </tr>
+            <tr>
+              <td>A</td>
+              <td>4.0</td>
+              <td>90-100</td>
+            </tr>
+            <tr>
+              <td>B+</td>
+              <td>3.5</td>
+              <td>80-89</td>
+            </tr>
+            <tr>
+              <td>B</td>
+              <td>3.0</td>
+              <td>70-79</td>
+            </tr>
+            <tr>
+              <td>C+</td>
+              <td>2.5</td>
+              <td>65-69</td>
+            </tr>
+            <tr>
+              <td>C</td>
+              <td>2.0</td>
+              <td>50-59</td>
+            </tr>
+            <tr>
+              <td>D+</td>
+              <td>1.5</td>
+              <td>45-49</td>
+            </tr>
+            <tr>
+              <td>D</td>
+              <td>1.0</td>
+              <td>40-44</td>
+            </tr>
+            <tr>
+              <td>F</td>
+              <td>0</td>
+              <td><40</td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
